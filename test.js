8ae1984ee5b388432b286664131447a1a1f6176a
DEN-2020 <72227799+DEN-2020@users.noreply.github.com>
2025-12-29 20:08:19 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,96 +2,66 @@
     'use strict';
 
     var plugin = {
-        component: 'test_plugin',
-        name: 'Test Skeleton',
-        icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" rx="12" fill="white"/></svg>'
+        component: 'test_debug',
+        name: 'Debug Tool'
     };
 
-    // --- ЛОГИКА СТРАНИЦЫ ---
-    function pluginPage(object) {
-        var html = $('<div></div>');
-        var scroll = new Lampa.Scroll({mask: true, over: true});
-        var body = $('<div class="category-full" style="padding: 20px;"></div>');
+    // Окно для вывода логов прямо на ТВ
+    var debugLog = $('<div id="debug-box" style="position: absolute; bottom: 10%; right: 5%; width: 300px; height: 200px; background: rgba(0,0,0,0.8); color: lime; font-size: 12px; overflow: hidden; z-index: 9999; border: 1px solid #fff; pointer-events: none; padding: 5px;">DEBUG LOG:<br></div>');
 
-        this.create = function () {
-            var _this = this;
-            body.append('<div class="selector" style="padding: 20px; background: rgba(255,255,255,0.1); border-radius: 10px;">Нажми на пульте цифры или влево/вправо</div>');
-            
-            // Добавим тестовый список
-            for(var i = 1; i <= 5; i++) {
-                var item = $('<div class="selector" style="padding: 10px; margin-top: 10px; border: 1px solid #fff;">Тестовый канал ' + i + '</div>');
-                body.append(item);
-            }
-
-            scroll.append(body);
-            html.append(scroll.render());
-            return this.render();
-        };
-
-        this.render = function () { return html; };
-        
-        this.start = function () {
-            Lampa.Controller.add('content', {
-                toggle: function () {
-                    Lampa.Controller.collectionSet(html);
-                    Lampa.Controller.follow('content');
-                },
-                left: function () { Lampa.Controller.toggle('menu'); },
-                up: function () { Lampa.Controller.toggle('head'); },
-                back: Lampa.Activity.backward
-            });
-            Lampa.Controller.toggle('content');
-        };
-
-        this.pause = function () {};
-        this.stop = function () {};
-        this.destroy = function () { scroll.destroy(); html.remove(); };
+    function log(msg) {
+        debugLog.append('<div>> ' + msg + '</div>');
+        if (debugLog.children().length > 10) debugLog.children().eq(1).remove();
+        console.log('Plugin Log:', msg);
     }
 
-    // --- ОБРАБОТЧИК КНОПОК (ПОЧЕМУ ИХ НЕТ) ---
-    function handleKeyDown(e) {
-        if (Lampa.Activity.active().component === plugin.component) {
-            console.log('Нажата кнопка:', e.code);
-            // Вывод уведомления прямо в Лампе при нажатии любой кнопки
-            Lampa.Noty.show('Нажат код: ' + e.code);
-            
-            if (e.code >= 48 && e.code <= 57) {
-                Lampa.Noty.show('Вы нажали цифру: ' + (e.code - 48));
-            }
-        }
-    }
-
-    // --- ЗАПУСК ---
     function startPlugin() {
-        if (window.test_plugin_ready) return;
-        window.test_plugin_ready = true;
+        if (window.test_debug_ready) return;
+        window.test_debug_ready = true;
 
-        // 1. Регистрация компонента
-        Lampa.Component.add(plugin.component, pluginPage);
+        $('body').append(debugLog);
+        log('Plugin Started');
 
-        // 2. Добавление в меню
-        var menu_item = $('<li class="menu__item selector" data-action="' + plugin.component + '">' +
-            '<div class="menu__ico">' + plugin.icon + '</div>' +
-            '<div class="menu__text">' + plugin.name + '</div>' +
-        '</li>');
-
-        menu_item.on('hover:enter', function () {
-            Lampa.Activity.push({
-                url: '',
-                title: plugin.name,
-                component: plugin.component,
-                page: 1
-            });
+        // --- ИССЛЕДУЕМ МЕНЮ ---
+        // Смотрим, какие списки вообще есть в приложении
+        var availableMenus = [];
+        $('.menu__list, .menu ul, .navigation').each(function() {
+            var className = $(this).attr('class') || 'no class';
+            availableMenus.push(className);
         });
+        log('Found menus: ' + availableMenus.join(' | '));
 
-        $('.menu .menu__list').append(menu_item);
+        // Безопасное добавление в меню (проверка на дубликаты)
+        if ($('.menu__item[data-action="' + plugin.component + '"]').length === 0) {
+            var menu_item = $('<li class="menu__item selector" data-action="' + plugin.component + '">' +
+                '<div class="menu__text">' + plugin.name + '</div>' +
+            '</li>');
 
-        // 3. Главный слушатель кнопок
-        Lampa.Key.listener.follow('keydown', handleKeyDown);
+            menu_item.on('hover:enter', function () {
+                Lampa.Noty.show('Debug component active');
+            });
+
+            // Пытаемся добавить в основной список
+            $('.menu .menu__list').append(menu_item);
+            log('Added to .menu__list');
+        } else {
+            log('Item already exists, skipping duplication');
+        }
+
+        // --- СЛУШАЕМ КНОПКИ ---
+        Lampa.Key.listener.follow('keydown', function(e) {
+            log('Key pressed: ' + e.code);
+        });
     }
 
-    // Ждем готовности приложения
+    // Регистрация компонента (пустая страница для теста)
+    Lampa.Component.add(plugin.component, function() {
+        this.create = function() { return $('<div></div>'); };
+        this.render = function() { return $('<div></div>'); };
+        this.start = function() { Lampa.Controller.toggle('menu'); };
+        this.pause = this.stop = this.destroy = function() {};
+    });
+
     if (window.appready) startPlugin();
     else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
-
 })();
